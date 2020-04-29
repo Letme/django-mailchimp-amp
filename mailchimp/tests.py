5fb16fb2855972b1434124485ce53d460655ecc9
Crt Mori <crt@the-mori.com>
2020-04-29 23:47:41 +0200

    Add tests for settings parsing

--- mailchimp/tests.py
+++ mailchimp/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from . import views
 
-# Create your tests here.
+class SettingsParseTestCase(TestCase):
+    '''
+    Test if parsing of the settings works as intended and that it overwrites the
+    defaults, but keeps them where they are not defined
+    '''
+    def test_confirm_default_success_message(self):
+        '''
+        Confirm default success message is not overwritten if not defined in
+        application settings
+        '''
+        test_settings = views.mailchimp_parse_settings()
+        self.assertEqual(test_settings['success_message'], views.default_settings['success_message'])
+
+    @override_settings(MAILCHIMP_MESSAGES = { 'success_message': 'Test' })
+    def test_overwrite_default_success_message(self):
+        '''
+        Confirm that we can overwrite default success message from application
+        settings. Also do not touch other fields.
+        '''
+        test_settings = views.mailchimp_parse_settings()
+        self.assertEqual(test_settings['success_message'], 'Test')
+        self.assertEqual(test_settings['error_member_exists'], views.default_settings['error_member_exists'])
+
+    def test_confirm_default_error_member_exists(self):
+        '''
+        Confirm default error message when member exists is not overwritten if
+        not defined in application settings
+        '''
+        test_settings = views.mailchimp_parse_settings()
+        self.assertEqual(test_settings['error_member_exists'], views.default_settings['error_member_exists'])
+
+    @override_settings(MAILCHIMP_MESSAGES = { 'error_member_exists': 'Test1' })
+    def test_overwrite_default_error_member_exists(self):
+        '''
+        Confirm that we can overwrite default error message when member exists
+        from application settings.
+        '''
+        test_settings = views.mailchimp_parse_settings()
+        self.assertEqual(test_settings['error_member_exists'], 'Test1')
+        self.assertEqual(test_settings['success_message'], views.default_settings['success_message'])
+
+
